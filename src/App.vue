2b58dcc442365fbe894dd1b33a53acb799b5ed20
任跃兵 <cnryb@live.cn>
2024-02-29 14:20:47 +0800

    修复类型

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
 	<div class="root-container">
 		<!-- <img src="chrome-search://ntpicon/?size=48@1.000000x&amp;url=https://wiki.zhaopin.com/"> -->
@@ -12,8 +8,10 @@
 <script setup lang="ts">
 import { onMounted,ref } from "vue";
 
+const mostVisitedURLs = ref<chrome.topSites.MostVisitedURL[]>([]);
+
 const test = ref('')
-function getFaviconUrl(url) {
+function getFaviconUrl(url: string) {
   let faviconUrl = new URL(`chrome-extension://${chrome.runtime.id}/_favicon/`);
   faviconUrl.searchParams.append('pageUrl', url);
   faviconUrl.searchParams.append('size', '32');
@@ -23,9 +21,9 @@ function getFaviconUrl(url) {
 test.value = getFaviconUrl('https://github.com/')
 onMounted(async () => {
 	console.log('App mounted');
-	const mostVisitedURLs:{title:string;url:string}[] = await chrome.topSites.get();
+	mostVisitedURLs.value = await chrome.topSites.get();
 
-	console.log(mostVisitedURLs);
+	console.log(mostVisitedURLs.value);
 });
 </script>
 
